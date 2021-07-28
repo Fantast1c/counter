a8fd9b1b8ff34580772fbfbf8a164e8dc430b27d
Nikolay Lim <limnikolay99@gmail.com>
2021-07-28 13:08:33 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+   let [state, setState] = useState(0);
+
+    const increment = ()=>{state<5 ? setState(state+1) :  setState(state) }
+    const reset = ()=>{state===5 ? setState(state=0): setState(state)}
+
+    const display = {color : state===5 ? "red":"#2e2e2e"}
+    const inc ={backgroundColor : state===5 ? "#1b746c":"#26a69a"}
+    const res ={backgroundColor : state===5 ? "#26a69a":"#1b746c"}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div className="Display" style={display}>
+           {state}
+        </div>
+        <div className="Container-button">
+          <span><button className="Button" style={inc} onClick={increment}>inc</button></span>
+            <span><button className="Button" style={res} onClick={reset}>reset</button></span>
+        </div>
+      </div>
     </div>
   );
 }
